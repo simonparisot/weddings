eb1ebe3868051bd5ea63cccbfc165746b6b28005
simonparisot <parisot.simon@gmail.com>
2020-09-08 01:20:59 +0200

    new checkout lambda

---
+++ LambdaPaymentStripe/v2/index.js
@@ -0,0 +1,34 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+
+	console.log("request: " + JSON.stringify(event));
+
+	if (event.body && event.body !== "") {
+    	let body = JSON.parse(event.body);
+	    if (body.priceid && body.priceid !== "") {
+	        var priceid = body.priceid;
+	    }
+	}
+
+	const session = await stripe.checkout.sessions.create({
+		payment_method_types: ["card"],
+		line_items: [
+			{price: priceid, quantity: 1},
+		],
+		mode: "payment",
+		success_url: "https://dianeetguillaume.fr/merci.html",
+		cancel_url: "https://dianeetguillaume.fr#liste",
+	});
+
+	let responseBody = { id: session.id };
+
+	let response = {
+        statusCode: 200,
+        body: JSON.stringify(responseBody)
+    };
+
+    console.log("response: " + JSON.stringify(response))
+    return response;
+
+}
