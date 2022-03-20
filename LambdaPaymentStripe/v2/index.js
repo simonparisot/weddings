d82e003d21d8bb7261a61ed549cb09783b86a44c
simonparisot <parisot.simon@gmail.com>
2022-03-20 23:44:37 +0100

    updated dianeetguillaume and new payment lambda

--- LambdaPaymentStripe/v2/index.js
+++ LambdaPaymentStripe/v2/index.js
@@ -3,23 +3,43 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 exports.handler = async (event) => {
 
 	console.log("request: " + JSON.stringify(event));
+	let price_item = new Object();
 
 	if (event.body && event.body !== "") {
     	let body = JSON.parse(event.body);
 	    if (body.priceid && body.priceid !== "") {
-	        var priceid = body.priceid;
+	    	// this is a standard product stored in Stripe
+	        // var priceid = body.priceid;
+	        price_item.price = body.priceid;
+	        price_item.quantity = 1;
+	    }else{
+	    	// this is a custom payment
+	    	price_item.price_data = {};
+	    	price_item.price_data.currency = "eur";
+	    	price_item.price_data.unit_amount = body.custom_price;
+	    	price_item.price_data.product_data = {};
+	    	price_item.price_data.product_data.name = "Cadeau personnalisé";
+	    	price_item.price_data.product_data.description = body.custom_text;
+	    	price_item.price_data.product_data.images = ["https://audreyetsimon.fr/naissance/img/list/surprise.jpg"];
+	        price_item.quantity = 1;
 	    }
 	}
 
-	const session = await stripe.checkout.sessions.create({
-		payment_method_types: ["card"],
-		line_items: [
-			{price: priceid, quantity: 1},
-		],
-		mode: "payment",
-		success_url: "https://audreyetsimon.fr/naissance/merci.html",
-		cancel_url: "https://audreyetsimon.fr/naissance/",
-	});
+	const session = await stripe.checkout.sessions.create(
+		{
+			payment_method_types: ["card"],
+			/* line_items: [
+				{
+					price: priceid, 
+					quantity: 1
+				},
+			], */
+			line_items: [price_item],
+			mode: "payment",
+			success_url: "https://audreyetsimon.fr/naissance/merci.html",
+			cancel_url: "https://audreyetsimon.fr/naissance/",
+		}
+	);
 
 	let responseBody = { id: session.id };
 
--- updateLambda.sh
+++ updateLambda.sh
@@ -2,4 +2,4 @@ cd $1
 zip -r code *
 mv code.zip ..
 cd ..
-aws lambda update-function-code --function-name $1 --zip-file "fileb://code.zip" --region eu-west-3
+aws lambda update-function-code --function-name $2 --zip-file "fileb://code.zip" --region eu-west-1 --profile perso
