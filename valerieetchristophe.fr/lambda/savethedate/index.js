9f9a33fe380dccd1be006acd18d9777f66d8f68a
simonparisot <parisot.simon@gmail.com>
2022-11-20 11:11:59 +0100

    removed code.zip

--- valerieetchristophe.fr/lambda/savethedate/index.js
+++ valerieetchristophe.fr/lambda/savethedate/index.js
@@ -7,9 +7,13 @@ exports.handler = function(event, context, callback){
     
     // console.log("DEBUG:" + "New request: " + JSON.stringify(event));
     var requestBody = JSON.parse(event.body);
-    var name = requestBody.name;
-    var howmany = requestBody.nb;
-    var address = requestBody.address;
+    var item ={};
+    item['ok'] = requestBody.ok;
+    item['name'] = requestBody.name;
+    item['creation-date'] = new Date().toISOString();
+    if (requestBody.nb) item['howmany'] = requestBody.nb;
+    if (requestBody.address) item['address'] = requestBody.address;
+
 
     store().then(() => {
 
@@ -36,14 +40,10 @@ exports.handler = function(event, context, callback){
     });
 
     function store() {
+        console.log("Storing:" + JSON.stringify(item));
         return ddb.put({
             TableName: process.env.TABLE_NAME,
-            Item: {
-                name: name,
-                howmany: howmany,
-                address: address,
-                RequestTime: new Date().toISOString(),
-            },
+            Item: item,
         }).promise();
     }
 
